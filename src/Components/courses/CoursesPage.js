d8bd2760801e412917759acab1e633ad5c2bb2fe
BalajiGanesh1 <46281523+BalajiGanesh1@users.noreply.github.com>
2019-03-01 21:03:27 +0530

    Add files via upload

---
+++ src/Components/courses/CoursesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+export class CoursesPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses Page</h1>
+      </div>
+    )
+  }
+}
+
+export default CoursesPage;
